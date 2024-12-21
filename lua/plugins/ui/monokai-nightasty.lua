4eda394914f9bf3607ddfd5e99041805c170fb93
Van Anh Giang <vangianganh@gmail.com>
2024-12-22 00:14:58 +0700

    new colorschemes

---
+++ lua/plugins/ui/monokai-nightasty.lua
@@ -0,0 +1,6 @@
+return -- Lazy
+{
+  "polirritmico/monokai-nightasty.nvim",
+  lazy = false,
+  priority = 1000,
+}
---
+++ lua/plugins/ui/ofirkai.lua
@@ -0,0 +1,7 @@
+
+return -- Lazy
+{
+  "ofirgall/ofirkai.nvim",
+  lazy = false,
+  priority = 1000,
+}
