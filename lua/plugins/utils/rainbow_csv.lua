8e80682cfeeded64e463691a94058b936f1349a5
Van Anh Giang <vangianganh@gmail.com>
2024-12-23 19:35:09 +0700

    feat: Add rainbow_csv plugin configuration

---
+++ lua/plugins/utils/rainbow_csv.lua
@@ -0,0 +1,7 @@
+return {
+    "mechatroner/rainbow_csv",
+    config = function()
+        -- No additional setup is required for basic functionality.
+        -- Optional: Add custom configurations if needed.
+    end,
+}
