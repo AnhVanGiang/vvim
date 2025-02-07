4ae93fc181c7778dcd6310992d4ae3d4bf13ec49
Van Giang <vangianganh@gmail.com>
2025-02-07 19:10:25 +0700

    Templates

---
+++ templates/bigquery_pd.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from google.cloud import bigquery, bigquery_storage
+
+client = bigquery.Client(project="tamara-44603")
+bqstorage_client = bigquery_storage.BigQueryReadClient()
+
+{{_cursor_}}
